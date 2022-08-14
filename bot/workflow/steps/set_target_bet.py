c41213f45b02b2ca7c922b009d7fbea2a35a3b6a
Kot Shrodingera <kot.shrodingera@gmail.com>
2022-08-14 04:51:49 +0200

    Фикс обработки param: null в ставке

--- bot/workflow/steps/set_target_bet.py
+++ bot/workflow/steps/set_target_bet.py
@@ -90,7 +90,7 @@ def set_target_bet(self: Workflow) -> None:
         'alternative_selection_name': None,
         'column': None,
         'minimum_coefficient': float(self.target_bet['max_lower_coef_percent']) if 'max_lower_coef_percent' in self.target_bet else 1.1,
-        'parameter': float(self.target_bet['param']) if 'param' in self.target_bet else None,
+        'parameter': float(self.target_bet['param']) if 'param' in self.target_bet and self.target_bet['param'] != None else None,
     }
     selection_details = self.target_bet['bet365_bet_name'].split('|')
     if len(selection_details) == 2:
