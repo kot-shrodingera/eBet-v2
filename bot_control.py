d3285db3f1a9b1f9700bb4a7ce73eee13ced0ed7
Kot Shrodingera <kot.shrodingera@gmail.com>
2022-08-20 19:29:32 +0200

    Обновление bot_control

--- bot_control.py
+++ bot_control.py
@@ -1,10 +1,18 @@
+import msvcrt
 import os
+import urllib.parse
+import requests
+import shutil
 import sys
 import subprocess
-import msvcrt
-from time import sleep, time
-from typing import List, Union
 
+from typing import List, Union
+from time import time, sleep
+from configparser import ConfigParser
+from zipfile import ZipFile
+from io import BytesIO
+
+settings_file_name = './settings.ini'
 control_filename = './control'
 
 def set_action(action: str) -> None:
@@ -30,7 +38,7 @@ def get_key(accepted_keys: List[str], timeout: float = 0.5) -> Union[str, None]:
             key = None
             break
     return key
-    
+
 def init() -> None:
     print('Initializing')
     print('[P] Pause Bot [S] Stop Bot')
@@ -90,22 +98,84 @@ def stop() -> None:
     print('Stopped')
     os.remove(control_filename)
 
+def get_current_version() -> str:
+    with open('current_version') as file:
+        return file.read()
+
+def set_current_version(version: str) -> None:
+    with open('current_version', 'w') as file:
+        return file.write(version)
+
+def get_last_version(api_key, api_password) -> str:
+    
+    query_data = {
+        'api[method]': 'get_bot_versions',
+        'api[version]': '1',
+        'api_key': api_key,
+        'password': api_password,
+    }
+    query_string = urllib.parse.urlencode(query_data)
+    
+    get_bot_versions_url = f'http://bvb.strike.ws/bot/index.php?{query_string}'
+    response = requests.get(get_bot_versions_url).json()
+    
+    if 'versions' in response and 'last' in response['versions'] and 'version' in response['versions']['last']:
+        last_version = response['versions']['last']['version']
+        return last_version
+    else:
+        raise Exception('No last version in response')
+
+def download_version(version: str) -> None:
+    get_bot_url = f'http://bvb.strike.ws/bot/soft/bot_versions/{version}.zip'
+    bor_response = requests.get(get_bot_url)
+    zip_file = ZipFile(BytesIO(bor_response.content))
+    path = f'./versions/{version}'
+    if os.path.exists(path):
+        print(f'Path {path} already exists. Overwriting')
+        shutil.rmtree(path)
+    zip_file.extractall(path)
+
 def main() -> None:
+    settings = ConfigParser()
+    settings.read(settings_file_name)
+    
+    api_key = settings['Settings']['api_key']
+    api_password = settings['Settings']['api_password']
+    bot_path = settings['Settings']['bot_path'] if 'bot_path' in settings['Settings'] else None
+    
+    
+    current_version = get_current_version()
+    print(f'Current Version: {current_version}')
+    
+    last_version = get_last_version(api_key, api_password)
+    print(f'Last Version: {last_version}')
+    
+    print('[R] Run Bot [U] Update Bot [Q] Quit')
+    key = get_key(['r', 'u', 'q'], 0)
+    if key == 'u':
+        download_version(last_version)
+        set_current_version(last_version)
+        current_version = get_current_version()
+    elif key == 'q':
+        return
+    elif key == 'r':
+        pass
+    else:
+        print(f'Unknown key [init] {key}')
+        return
+    
+    # TODO: check control file and suggest continue or delete
+    
     write_pyc = len(sys.argv) > 1 and sys.argv[1] == '-B'
+    python_venv_path = './venv/python3.8/Scripts/python.exe'
+    python_path = python_venv_path if os.path.exists(python_venv_path) else 'python.exe'
+    if bot_path is None:
+        bot_path = f'./versions/{current_version}/main.py'
+    arguments = [python_path, '-B', bot_path] if write_pyc else [python_path, bot_path]
+
     if not os.path.exists(control_filename):
-        print('[R] Run Bot [Q] Quit')
-        key = get_key(['r', 'q'], 0)
-        if key == 'r':
-            set_action('init')
-            python_venv_path = './venv/python3.8/Scripts/python.exe'
-            python_path = python_venv_path if os.path.exists(python_venv_path) else 'python.exe'
-            arguments = [python_path, '-B', './main.py'] if write_pyc else [python_path, './main.py']
-            subprocess.Popen(arguments, creationflags=subprocess.CREATE_NEW_CONSOLE)
-        elif key == 'q':
-            return
-        else:
-            print(f'Unknown key [init] {key}')
-            return
+        set_action('init')
+        subprocess.Popen(arguments, creationflags=subprocess.CREATE_NEW_CONSOLE)
     while True:
         current_action = get_action()
         if current_action == 'init':
