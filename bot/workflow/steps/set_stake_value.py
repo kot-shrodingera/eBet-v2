7cc7ed2d54582ce0d636d8387cf0e29a84310689
Kot Shrodingera <kot.shrodingera@gmail.com>
2022-09-11 23:25:43 +0200

    Дополнительные логи при расчёте суммы ставки

--- bot/workflow/steps/set_stake_value.py
+++ bot/workflow/steps/set_stake_value.py
@@ -21,8 +21,10 @@ def set_stake_value(self: Workflow) -> None:
     elif self.settings.stake_type == 'percent':
         balance = get_balance(self.browser)
         if self.settings.stake_max is None:
+            logger.log('No Maximum stake')
             base_target_stake_value = round(balance['balance'] * self.settings.stake / 100, 2)
         else:
+            logger.log('Maximum stake is {self.settings.stake_max}')
             base_target_stake_value = min(round(balance['balance'] * self.settings.stake / 100, 2), self.settings.stake_max)
     else:
         raise BotError(f'Unknown stake type: {self.settings.stake_type}')
