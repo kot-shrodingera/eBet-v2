cd4e7692aab54b1dd7a40929eb179e0b8c079cb3
Kot Shrodingera <kot.shrodingera@gmail.com>
2022-10-07 10:21:09 +0200

    Рефактор проверки ставки

--- bot/bot.py
+++ bot/bot.py
@@ -17,7 +17,7 @@ class Bot:
     settings: Settings
     browser: Browser
     control: Control
-    bot_version = '2.0.12'
+    bot_version = '2.0.13'
     
     ebet_auth_token: str
     first_launch: bool
--- bot/workflow/steps/check_bet.py
+++ bot/workflow/steps/check_bet.py
@@ -6,22 +6,41 @@ from ...errors import BotError
 
 def check_bet(self: Workflow, initial: bool = False) -> None:
     minimum_coefficient = self.bet_details['minimum_coefficient']
-    current_coefficient = bet365.get_coefficient(self.browser)
-    if initial:
-        self.initial_coefficient = current_coefficient
+    maximum_coefficient = self.bet_details['maximum_coefficient']
 
     target_parameter = self.bet_details['parameter']
-    current_parameter = bet365.get_parameter(self.browser)
-    if initial:
-        self.initial_parameter = current_parameter
+    
+    if 'score1' not in self.target_bet:
+        raise BotError('No score1 in target bet')
+    target_score1 = self.target_bet['score1']
+    if not target_score1.isnumeric():
+        raise BotError(f'score1 in target bet is not numeric: {target_score1}')
+    if 'score2' not in self.target_bet:
+        raise BotError('No score2 in target bet')
+    target_score2 = self.target_bet['score2']
+    if not target_score2.isnumeric():
+        raise BotError(f'score2 in target bet is not numeric: {target_score2}')
+
+    logger.log(f'Minimum coefficient: {minimum_coefficient} | Maximum coefficient: {maximum_coefficient} | Target parameter: {target_parameter} | Target Score: {target_score1}-{target_score2}')
 
     current_balance = bet365.get_balance(self.browser)
     if initial:
         self.balance_before_place_bet = bet365.get_balance(self.browser)
+    
+    current_coefficient = bet365.get_coefficient(self.browser)
+    if initial:
+        self.initial_coefficient = current_coefficient
+
+    current_parameter = bet365.get_parameter(self.browser)
+    if initial:
+        self.initial_parameter = current_parameter
 
     first_score, second_score = bet365.get_score(self.browser)
     
     logger.log(f'Balance: {current_balance["balance"]} | Coefficient: {current_coefficient} | Parameter: {current_parameter} | Score: {first_score}-{second_score}')
+    
+    if current_balance['balance'] < self.target_stake_value:
+        raise BotError('Balance is less than target stake value')
     
     if current_coefficient < minimum_coefficient:
         raise BotError('Current coefficient is lower than minimum')
@@ -31,21 +50,6 @@ def check_bet(self: Workflow, initial: bool = False) -> None:
         
     if current_parameter != target_parameter:
         raise BotError('Parameter has changed')
-    
-    if current_balance['balance'] < self.target_stake_value:
-        raise BotError('Balance is less than target stake value')
-    
-    if 'score1' not in self.target_bet:
-        raise BotError('No score1 in target bet')
-    target_score1 = self.target_bet['score1']
-    if 'score2' not in self.target_bet:
-        raise BotError('No score2 in target bet')
-    target_score2 = self.target_bet['score2']
-    
-    if not self.target_bet['score1'].isnumeric():
-        raise BotError(f'score1 in target bet is not numeric: {target_score1}')
-    if not self.target_bet['score2'].isnumeric():
-        raise BotError(f'score2 in target bet is not numeric: {target_score2}')
     
     if first_score != int(target_score1) or second_score != int(target_score2):
         raise BotError(f'Not target score {target_score1}-{target_score2}')
