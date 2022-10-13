349e73cdc0daed7833dddc6fa4a666cba69d7b5f
Kot Shrodingera <kot.shrodingera@gmail.com>
2022-10-13 14:37:50 +0200

    Новый класс ошибки

--- bot/errors.py
+++ bot/errors.py
@@ -1,2 +1,17 @@
+from enum import Enum
+from typing import Dict, Union
+
+class ErrorType(Enum):
+    UNKNOWN = 1
+    WRONG_BET_OPENED = 2
+
 class BotError(Exception):
-    pass
+    message: str
+    error_type: ErrorType
+    data: Union[Dict, None]
+    
+    def __init__(self, message: str, type: ErrorType = ErrorType.UNKNOWN, data: Union[Dict, None] = None):
+        self.message = message
+        super().__init__(self.message)
+        self.type = type
+        self.data = data
