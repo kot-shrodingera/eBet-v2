f6a020c04a8a9ed90230696e9892ef3d27c1790e
Kot Shrodingera <kot.shrodingera@gmail.com>
2022-12-11 21:01:36 +0100

    Опция js_scroll

--- bot/browser/node/_scroll_to.py
+++ bot/browser/node/_scroll_to.py
@@ -4,13 +4,20 @@ from timeout import random_timeout
 
 from . import Node
 
+scroll_function = '''
+(scrollAmount, scrollElementSelector) => {
+    const element = document.querySelector(scrollElementSelector);
+    element.scrollTop += scrollAmount;
+}
+'''
+
 def scroll_to(self: Node, element_coordinate=None, container_css_selector=None, scrollable_section_css_selector=None, amount=None) -> int:
     if container_css_selector:
         visible_section_top_coordinate, visible_section_bottom_coordinate = self.browser.crdi.move_mouse_to_container(container_css_selector)
     else:
         visible_section_top_coordinate = 0
         response = self.browser.crdi.execute_javascript_statement('window.innerHeight')
-        visible_section_bottom_coordinate = response['value']
+        visible_section_bottom_coordinate = response['value'] # pyright: ignore [reportGeneralTypeIssues]
 
     if element_coordinate: # Default option
         # print('Passed coordinate:', coordinate)
@@ -27,6 +34,12 @@ def scroll_to(self: Node, element_coordinate=None, container_css_selector=None, 
             coordinate_to_scroll_to = element_coordinate - visible_section_bottom_coordinate
             # print('coordinate_to_scroll_to:', coordinate_to_scroll_to)
             # print()
+            
+            if self.browser.settings.js_scroll:
+                scroll_amount = 100 * (math.ceil(coordinate_to_scroll_to / 100) + 1)
+                self.browser.run_js_function(scroll_function, [scroll_amount, container_css_selector or 'body'])
+                return scroll_amount
+            
             steps = math.ceil(coordinate_to_scroll_to / 100)
             if not scrollable_section_css_selector: # If there isn't a chance to overscroll, add randomisation
                 steps_randomiser = random.randint(0, 1)
@@ -46,6 +59,12 @@ def scroll_to(self: Node, element_coordinate=None, container_css_selector=None, 
             return scroll_amount
         elif element_coordinate < visible_section_top_coordinate: # Scroll up
             coordinate_to_scroll_to = (element_coordinate - visible_section_top_coordinate) * -1
+            
+            if self.browser.settings.js_scroll:
+                scroll_amount = -100 * (math.ceil(coordinate_to_scroll_to / 100) + 1)
+                self.browser.run_js_function(scroll_function, [scroll_amount, container_css_selector or 'body'])
+                return scroll_amount
+            
             steps = math.ceil(coordinate_to_scroll_to / 100) # We may need to scroll to the top, so using a randomiser could cause us to overscroll despite having reached the top meaning the coordinate would be wrong
             for step in range(steps):
                 self.browser.crdi.Input_dispatchMouseEvent(type='mouseWheel', x=self.browser.crdi.x_coordinate, y=self.browser.crdi.y_coordinate, deltaX=-1, deltaY=-100, pointerType='mouse')
--- bot/settings.py
+++ bot/settings.py
@@ -34,6 +34,7 @@ class Settings:
     disable_refresh_balance: bool
     show_click_coords: bool
     narrow_click_coords: float
+    js_scroll: bool
     
     window_width: Optional[int]
     window_height: Optional[int]
@@ -103,6 +104,7 @@ class Settings:
         set_property('disable_refresh_balance', 'disable_refresh_balance', bool, default=False)
         set_property('show_click_coords', 'show_click_coords', bool, default=False)
         set_property('narrow_click_coords', 'narrow_click_coords', float, default=1) # TODO: validate [0.0 - 1.0]
+        set_property('js_scroll', 'js_scroll', bool, default=False)
         
         set_property('window_width', 'window_width', int, required=False)
         set_property('window_height', 'window_height', int, required=False)
