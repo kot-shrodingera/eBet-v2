e8bd3365b915a5f394b683b6af9504d57744963f
Kot Shrodingera <kot.shrodingera@gmail.com>
2022-12-14 18:15:38 +0100

    Исправление очистки купона

--- bot/bot.py
+++ bot/bot.py
@@ -17,7 +17,7 @@ class Bot:
     settings: Settings
     browser: Browser
     control: Control
-    bot_version = '2.0.36'
+    bot_version = '2.0.37'
     
     ebet_auth_token: str
     first_launch: bool
--- bot/workflow/steps/clear_betslip.py
+++ bot/workflow/steps/clear_betslip.py
@@ -7,42 +7,74 @@ from ... import logger
 from ...errors import BotError, ErrorType
 
 
+betslip_selector = '.bss-StandardBetslip:not(.bss-StandardBetslip-hidden)'
 
-bet_count_selector = '.bss-DefaultContent_BetCount'
-clear_betslip_button_selector = '.bss-RemoveButton, .bss-ReceiptContent_Done'
+betslip_single_selector = '.bss-StandardBetslip.bs-AnimationHelper_Single'
+betslip_collapsed_selector = '.bss-StandardBetslip.bs-AnimationHelper_Collapsed' # same match
+betslip_condensed_selector = '.bss-StandardBetslip.bs-AnimationHelper_Condensed' # different matches
+
+receipt_done_button_selector = '.bss-ReceiptContent_Done'
+remove_bet_button_selector = '.bss-RemoveButton'
 collapsed_betslip_selector = '.bss-StandardBetslip.bs-AnimationHelper_Collapsed'
 expanded_betslip_selector = '.bss-StandardBetslip.bs-AnimationHelper_Expanded'
-remove_bet_button_selector = '.bss-RemoveButton'
+
+def is_clear_betslip(self: Workflow) -> bool:
+    return not self.browser.node('Betslip Any', betslip_selector, 1, required=False)
 
 def clear_betslip(self: Workflow) -> None:
     after_clear_delay = 0.5
-    bet_count = self.browser.node('Bet Count', bet_count_selector, 1)
-    bet_count_int = int(bet_count.get_property('textContent'))
-    logger.log(f'Bet count: {bet_count_int}')
     
-    if bet_count_int == 0:
+    betslip = self.browser.node('Betslip', betslip_selector, 1, required=False)
+    if not betslip:
         logger.log('Betslip is empty')
         return
     
-    clear_betslip_button = self.browser.node('Clear Betslip Button', clear_betslip_button_selector, 1, required=False)
-    if clear_betslip_button and clear_betslip_button.is_visible():
-        logger.log(f'Clearing Betslip and waiting {after_clear_delay}s...')
-        clear_betslip_button.click()
+    receipt_done_button = self.browser.node('Receipt Done Button', receipt_done_button_selector, 1, required=False)
+    if receipt_done_button:
+        logger.log(f'Removing bet and waiting {after_clear_delay}s...')
+        receipt_done_button.click()
         sleep(after_clear_delay)
+        if not is_clear_betslip(self):
+            raise BotError('Could not clear receipt done betslip', ErrorType.COULD_NOT_CLEAR_BETSLIP)
+        logger.log('Betslip cleared')
         return
-
-    logger.log('No clear betslip button. Maybe collapsed betslip')
-    collapsed_betslip = self.browser.node('Collapsed Betslip', collapsed_betslip_selector, 1)
-    logger.log('Expanding collapsed betslip')
-    collapsed_betslip.click()
-    self.browser.node('Expanded Betslip', expanded_betslip_selector)
     
-    while remove_bet_button := self.browser.node('Remove Bet Button', remove_bet_button_selector, 1, required=False):
+    betslip_single = self.browser.node('Betslip Single', betslip_single_selector, 1, required=False)
+    if betslip_single:
+        remove_bet_button = self.browser.node('Remove Bet Button', remove_bet_button_selector, 1)
         logger.log(f'Removing bet and waiting {after_clear_delay}s...')
         remove_bet_button.click()
         sleep(after_clear_delay)
+        if not is_clear_betslip(self):
+            raise BotError('Could not clear single betslip', ErrorType.COULD_NOT_CLEAR_BETSLIP)
+        logger.log('Betslip cleared')
+        return
     
-    if float(bet_count.get_property('textContent')) != 0:
-        raise BotError('Could not clear betslip', ErrorType.COULD_NOT_CLEAR_BETSLIP)
+    betslip_collapsed = self.browser.node('Betslip Collapsed', betslip_collapsed_selector, 1, required=False)
+    if betslip_collapsed:
+        logger.log('Expanding collapsed betslip')
+        betslip_collapsed.click()
+        self.browser.node('Betslip Expanded', expanded_betslip_selector)
+        
+        while remove_bet_button := self.browser.node('Remove Bet Button', remove_bet_button_selector, 1, required=False):
+            logger.log(f'Removing bet and waiting {after_clear_delay}s...')
+            remove_bet_button.click()
+            sleep(after_clear_delay)
+        if not is_clear_betslip(self):
+            raise BotError('Could not clear collapsed betslip', ErrorType.COULD_NOT_CLEAR_BETSLIP)
+        logger.log('Betslip cleared')
+        return
     
-    logger.log('Betslip is cleared')
+    betslip_condensed = self.browser.node('Betslip Condensed', betslip_condensed_selector, 1, required=False)
+    if betslip_condensed:
+        remove_bet_button = self.browser.node('Remove Bet Button', remove_bet_button_selector, 1)
+        logger.log(f'Removing bet and waiting {after_clear_delay}s...')
+        remove_bet_button.click()
+        sleep(after_clear_delay)
+        if not is_clear_betslip(self):
+            raise BotError('Could not clear condensed betslip', ErrorType.COULD_NOT_CLEAR_BETSLIP)
+        logger.log('Betslip cleared')
+        return
+    
+    raise BotError('Could not clear betslip. Unknown betslip type', ErrorType.COULD_NOT_CLEAR_BETSLIP)
+    
